d70052bcdb17924e3811a58fc4f4c9a69e89e95f
mingooddev <mingood92@gmail.com>
2018-04-05 18:38:21 +0900

    Update SecurityConfig update guest permit

--- src/main/java/mybatis/hackday/config/SecurityConfig.java
+++ src/main/java/mybatis/hackday/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/guest/**").permitAll()
                 .antMatchers("/**").authenticated();
 
         http.csrf().disable();
