222047f800d275964c10195afc9a81fd0f4382d8
mingooddev <mingood92@gmail.com>
2018-04-05 18:36:47 +0900

    Add SecurityConfig

---
+++ src/main/java/mybatis/hackday/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package mybatis.hackday.config;
+
+import mybatis.hackday.service.MyAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyAuthenticationProvider myAuthenticationProvider;
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").authenticated();
+
+        http.csrf().disable();
+
+        http.formLogin()
+            .loginProcessingUrl("/guest/login_processing");
+
+        http.logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("user/logout_processing"))
+            .invalidateHttpSession(true);
+    }
+
+}
